12ad405de95949758705b12fa5e30d325ffa50b5
Sinan Anwar <sinan.anwar@hotmail.com>
2022-12-07 15:54:39 +1100

    Add files via upload

---
+++ ALACHATBOT.py
@@ -0,0 +1,41 @@
+print('Hello ALA team.I am Sinan64, i am chatbot')
+print('i completed 30 python algorithsm and have created simple python automation progarms')
+name = input('but 1st!, i know its ALA team, What is your name?: ')
+print('Hello', name, ', Nice to meet you, i was tasked to showcase my accomplished python tasks for my application, such as this chatbot, i was tasked to showcase 3 things guess how many i did?  ')
+
+ # conversation about feelings
+feeling = input('How are you feeling today?: ') print('Why are you feeling', feeling, 'now?') reason = input('Please tell me: ')
+print('I understand. Thanks for sharing')
+
+# G year information
+guessTasks = input('I am not very good at remembering how many tasks i was given, was it 3?: ')
+print(guessTasks. "maybe i shouldve kept it simple")
+print('well, heres a forked repository of 30 python algorithms ive completed over the past 2 weeks, maybe i shouldve kept it simple ')
+
+# ask user to guess age
+guessApp = input('Can you guess what other tasks ive completed?? - enter a \ number: ')
+print('i automated my emails using a python scripting program but maybe i shouldve kept it as, ', guessApp)
+
+# do math to calculate when chatbot will be 100
+myage = int(myage)
+nyears = 100 - myage
+print('I will be 100 in', nyears, 'years')
+print('That will be the year', int(year) + \ nyears)
+
+#basic icebreaker conversation
+print('I love games and for task 2 i created a simple connect 4 game')
+food = input('How about you? do like problem solving: ')
+print('I like', food, 'too.')
+
+
+question = 'would you like to change the focus to another application taks ' + guessApp + '?: '
+howoften = input(question)
+print('ive also completed a NUKE vfx automation scripting software . I wonder if that is good for your health')
+  # explanation conversation
+explanation = input('My favorite python algorithm was #211, . What is yours?: ')
+print(explanation,'! would you like an explanation of any of the algorithm task.')
+print('I wonder if like an', explanation, '', guessTasks, '?')
+
+# goodbye
+print('It has been a long day')
+print('I am too tired to talk. We can chat again later.') print('Goodbye', name, 'I liked chatting with you')
